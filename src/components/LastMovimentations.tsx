f3848f3cd15634170994beb914711d788948a182
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-27 01:32:04 -0300

    fix: change map display to reverse

--- src/components/LastMovimentations.tsx
+++ src/components/LastMovimentations.tsx
@@ -1,14 +1,17 @@
 import { useAccount } from "../hooks/account";
 import { MovimentationCard } from "./MovimentationCard";
-import "../styles/LastMovimentations.css"
+import "../styles/LastMovimentations.css";
 
 export function LastMovimentations() {
   const { movimentations } = useAccount().getInformation();
   return (
     <div className="LastMovimentations">
-      {movimentations.map((movimentation) => (
-        <MovimentationCard movimentation={movimentation} />
-      ))}
+      {movimentations
+        .slice(0)
+        .reverse()
+        .map((movimentation) => (
+          <MovimentationCard movimentation={movimentation} />
+        ))}
     </div>
   );
 }
