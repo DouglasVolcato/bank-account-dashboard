e6f98fc250b2ef637132a766db18ef8776263341
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-26 23:40:15 -0300

    feat: add last operation

--- src/components/LastOperation.tsx
+++ src/components/LastOperation.tsx
@@ -1,3 +1,33 @@
+import { useAccount } from "../hooks/account";
+import "../styles/LastOperation.css";
+
 export function LastOperation() {
-  return <div className="LastOperation">LastOperation</div>;
+  const { description, type, value } =
+    useAccount().getInformation().movimentations[0];
+
+  function cardColor() {
+    return type.toUpperCase() === "DEPOSIT"
+      ? "LastOperation greenCard"
+      : "LastOperation redCard";
+  }
+
+  return (
+    <div className={cardColor()}>
+      <h3 className="LastOperation-title">Last Movimentation:</h3>
+      <div className="LastOperation-card">
+        <p>
+          <div className="LastOperation-label">Type: </div>
+          <div className="LastOperation-value">{type}</div>
+        </p>
+        <p>
+          <div className="LastOperation-label">Value: </div>
+          <div className="LastOperation-value">${value.toFixed(2)}</div>
+        </p>
+        <p>
+          <div className="LastOperation-label">Description: </div>
+          <div className="LastOperation-value">{description}</div>
+        </p>
+      </div>
+    </div>
+  );
 }
