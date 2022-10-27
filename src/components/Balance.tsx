98b529728a349f90693d9ecfc88039bbbaaec0df
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-26 23:39:16 -0300

    feat: add balance

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,6 +1,23 @@
 import { useAccount } from "../hooks/account";
+import "../styles/Balance.css";
 
 export function Balance() {
-  const totalBalance = useAccount().getInformation().balance;
-  return <div className="Balance">Balance: {totalBalance}</div>;
+  const { balance, totalMovimented } = useAccount().getInformation();
+
+  function cardColor() {
+    return balance > 0 ? "Balance greenCard" : "Balance redCard";
+  }
+
+  return (
+    <div className={cardColor()}>
+      <p>
+        <div className="Balance-label">Balance:</div>
+        <div className="Balance-value">${balance.toFixed(2)}</div>
+      </p>
+      <p>
+        <div className="Balance-label">Total movimented: </div>
+        <div className="Balance-value">${totalMovimented.toFixed(2)}</div>
+      </p>
+    </div>
+  );
 }
